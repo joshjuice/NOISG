064b023ac837a2e8327a4b5eb227c772eaf95876
JoshJuice <93560041+joshjuice@users.noreply.github.com>
2026-02-21 23:59:41 +0800

    Create hungrycats.cpp

---
+++ 2026/Qualification/hungrycats.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define ppf pop_front
+#define ppb pop_back
+#define pf push_front
+#define pb push_back
+#define fr first
+#define sc second
+#define mnto(x,y) x = min(x, (__typeof__(x))y)
+#define mxto(x,y) x = max(x, (__typeof__(x))y)
+#define setval(arr, x) memset(arr, x, sizeof(arr))
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n;
+  cin >> n;
+  set<ll> ms;
+  vector<ll> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    ms.insert(a[i]);
+  }
+  sort(a.begin(), a.end());
+  if (ms.size() != n) {
+    cout << "NO";
+    return 0;
+  }
+  for (int i = 1; i < n; ++i) {
+    if (a[i] <= a[i-1]) {
+      cout << "NO";
+      return 0;
+    }
+    a[i]++;
+  }
+  cout << "YES";
+}
