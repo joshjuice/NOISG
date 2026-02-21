c92b9e14d63087cb06caddbac892b7ae8c7efccf
JoshJuice <93560041+joshjuice@users.noreply.github.com>
2026-02-22 00:02:47 +0800

    Update tourist.cpp

--- 2024/Qualification/tourist.cpp
+++ 2024/Qualification/tourist.cpp
@@ -1,1 +1,15 @@
-
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n, x, y; cin >> n >> x >> y;
+	deque<int> trips(n);
+	int sum = 0;
+	for (int i = 0; i < n; ++i) {
+		int a; cin >> a;
+		sum += min((a*x),y);
+	}
+	cout << sum;
+	return 0;
+}
