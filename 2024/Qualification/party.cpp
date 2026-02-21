d5d3670c1138724b9f6fc385ccc450af0f4ce143
JoshJuice <93560041+joshjuice@users.noreply.github.com>
2026-02-22 00:02:58 +0800

    Update party.cpp

--- 2024/Qualification/party.cpp
+++ 2024/Qualification/party.cpp
@@ -1,1 +1,21 @@
-
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main() {
+	int n, sum = 0;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	sort(a.rbegin(), a.rend());
+	if (n%2 == 1) {
+		n += 1;
+	}
+	for (int i = 0; i < n/2; ++i) {
+		if (a[i] > 0) {
+			sum += a[i];
+		}
+	}
+	cout << sum;
+}
